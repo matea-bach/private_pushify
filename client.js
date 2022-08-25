fa8e42c41ad3f2a14acb75d7bf697b49778e4d9b
Matea Bach <matea.bach@gmail.com>
2022-08-25 13:40:53 -0400

    open websocket connection

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const WebSocketClient = require("ws");
+const WebSocket = require("ws");
 require("dotenv").config();
 const axios = require("axios").default;
 const fs = require("fs");
@@ -19,13 +19,11 @@ const {
 
 async function main() {
   await axios
-    .get(`${URL}/application/${APPID_INTAKE}/message`, {
+    .get(`${URL}/stream`, {
       timeout: TIMEOUT,
       headers: {
         "Content-Type": "application/json",
         "X-Gotify-Key": CLIENT_TOKEN,
-        Connection: "Upgrade",
-        Upgrade: "websocket",
       },
     })
     .then((response) => {
@@ -44,9 +42,19 @@ async function main() {
         deleteMsg(msg.id, CLIENT_TOKEN);
       }
     })
-    .catch((err) => console.log("Response error", err));
+    .catch((err) => console.log("CATCH Response error", err.response));
 }
-main();
+// main();
+
+let socket = new WebSocket(`wss://push.4redbuttons.dev/stream`, {
+  headers: { "X-Gotify-Key": CLIENT_TOKEN },
+});
+
+socket.on("open", function open() {
+  console.log("WEBSOCKET OPENED");
+});
+
+//The stream endpoint is not included inside the api client because it is not definable via the swagger 2.0 specification. You can use any websocket client library to connect to the /stream endpoint with a client token.
 
 //checking for duplicates
 const alreadyInFile = function (msgBody) {
@@ -85,6 +93,6 @@ const deleteMsg = function (msgId, token) {
     })
     .then(console.log("Deleting message with id:", msgId))
     .catch((err) => {
-      console.log("deleteMsg error", err.response.status);
+      console.log("deleteMsg error", err.response);
     });
 };
